4b9ab1b6a98222988edff93181518f470e172389
Delpreti <rddesiqueira@gmail.com>
2019-11-12 11:40:16 -0300

    trabalho de orginfo

--- pagina0.js
+++ pagina0.js
@@ -1,3 +1,24 @@
 //Site em js
-var x = confirm("Press a button!");
+function enableLogin() {
+    let x = document.getElementById("formLogin");
+    x.hidden = false;
+}
 
+function confirmForm() {
+    let x = window.confirm("Login realizado com sucesso!");
+}
+
+function toggle(boolean) {
+    if(boolean){
+        return false;
+    }
+    return true;
+}
+
+function togglediv(number){
+    //funciona mas funciona errado
+    let x = document.getElementById("Domesticos");
+    if(number == 1){
+        x.hidden = toggle(x.hidden);
+    }
+}
