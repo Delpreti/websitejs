7940b9513cce200c5f4fd9203b7b091f5fa8a8cb
Delpreti <rddesiqueira@gmail.com>
2019-11-27 11:36:05 -0300

    tableview e tela de login melhorados

--- pagina0.js
+++ pagina0.js
@@ -1,7 +1,7 @@
 //Site em js
 function enableLogin() {
     var winx = window.open("login.html", 'pagina',
-    "width=600, height=500, top=100, left=110, scrollbars=no " );
+    "width=600, height=450, top=100, left=110, scrollbars=no " );
 }
 
 function confirmForm() {
@@ -25,15 +25,15 @@ function toggle(bool) {
     return true;
 }
 
-function togglediv(number){
-    //funciona mas funciona errado
+function togglediv(number) {
+
+    // pega um array com as divs
     let arr = [];
     arr.push(document.getElementById("Domesticos"));
     arr.push(document.getElementById("Residuos"));
     arr.push(document.getElementById("Rodados"));   
     
-
-
+    // pega um array com as setinhas das divs
     let imgarr = [];
     imgarr.push(document.getElementById("seta1"));
     imgarr.push(document.getElementById("seta2"));
@@ -41,18 +41,19 @@ function togglediv(number){
 
     let angle;
     let goingDown;
-    let slide;
-
-    if(arr[number].hidden) {
-        slide = 0;
+    let zoom;
+    
+    if(arr[number].hidden == true) { // Se a div esta escondida
+        zoom = 0;
         angle = 0;
-        goingDown = true;
-    } else {
+        goingDown = true; // ela irá descer
+    } else { // Se a div esta aparecendo
         angle = 90;
-        slide = 1;
-        goingDown = false;
+        zoom = 1;
+        goingDown = false; // ela irá subir
     }
 
+    // Se a div esta escondida, ela deixa de estar no momento do clique
     if(angle <= 0){
         arr[number].hidden = toggle(arr[number].hidden);
     }
@@ -60,17 +61,18 @@ function togglediv(number){
     function spin(bool) {
 
         // Incrementa o angulo da seta
-        if(bool) {
+        if(!bool) {
             angle -= 5;
-            slide -= 0.06;
+            zoom -= 0.06;
         } else {
             angle += 5;
-            slide += 0.06;
+            zoom += 0.06;
         }
 
-        // Printa a seta rotacionada
+        // Rotaciona a seta e ajusta um pouco.
         imgarr[number].setAttribute("style","transform:rotate(" + angle + "deg) translateX(" + (angle/10) + "px)");
-        arr[number].setAttribute("style","transform:scaleY(" + slide +")");
+        // "Abre" a div
+        arr[number].setAttribute("style","transform:scaleY(" + zoom +")");
 
         // Verifica se pode parar de girar
         if(angle >= 90 || angle <= 0) {
@@ -83,5 +85,6 @@ function togglediv(number){
 
     }
 
-    var timer = setInterval(spin, 10, arr[number].hidden);
+    // Chamamos a funcao spin(bool) a cada 10ms para executar a animacao de abrir e fechar a div
+    var timer = setInterval(spin, 10, goingDown);
 }
